00b6dfc0668f410dce72bd3694d7313d6acbafe8
aryark <aryark3112@gmail.com>
2023-07-18 12:28:54 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _18JulyDirectoryEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fFolder = "D:\\";
+            string newDir;
+            Console.WriteLine("Enter New Directory Name ");
+            newDir = Console.ReadLine();
+            string fPath = fFolder + newDir;
+            DirectoryInfo directoryInfo = new DirectoryInfo(fPath);
+            if(directoryInfo.Exists)
+            {
+                Console.WriteLine($"Directory {newDir} already exist");
+            }
+            else
+            {
+                directoryInfo.Create();
+                Console.WriteLine($"Directory {newDir} craetion on {fPath} success");
+            }
+            Console.ReadKey();
+        }
+    }
+}
